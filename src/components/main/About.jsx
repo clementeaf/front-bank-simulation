f0a13025ca7c2c75104dd11f2a4a5653de57831f
clementeaf <ext_carriagada@sbpay.cl>
2024-02-18 20:33:07 -0300

    First home content

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
-    return (
-      <div className="flex w-full h-full items-center justify-center">
-        About the Bank
-      </div>
-    );
-  }
+  return (
+    <div className="flex w-full h-4/5 items-center justify-evenly">
+      <AboutText />
+      <AboutImage />
+    </div>
+  );
+}
+
+function AboutText() {
+  return (
+    <div>
+      <h1 className="text-[60px]">Hero Title</h1>
+      <h5 className="text-[35px]">Hero SubTitle</h5>
+      <p className="text-[20px] w-[320px] text-start">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore quisquam porro voluptas provident dicta quibusdam excepturi maiores explicabo corrupti nostrum nam vel, alias consectetur? Harum?</p>
+    </div>
+  );
+}
+
+function AboutImage() {
+  return (
+    <>
+    <div className="w-80 h-80 bg-slate-400" />
+    </>
+  )
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/main/Footer";
 
 export default function Main() {
   return (
-    <div className="flex flex-col bg-slate-100 h-screen w-screen p-4 gap-3">
+    <div className="flex flex-col bg-slate-100 h-screen w-screen p-4 justify-between">
       <Header />
       <About />
       <Footer />
